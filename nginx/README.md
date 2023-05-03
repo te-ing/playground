056115541077763a3ed12e22446a9713652a17e1
taejung.kim <taejung.kim@snplab.io>
2023-05-03 20:28:12 +0900

    init: nginx 테스트를 위한 개발환경 설정

---
+++ nginx/README.md
@@ -0,0 +1,9 @@
+# nginx 테스트
+
+<br>
+
+## serve를 사용하여 로컬에서 npm run build 한 파일 사용하기
+### 설치 명령어
+> npm install -g serve
+### 실행 명령어
+> serve -s build -l 3000(원하는 포트)
---
+++ nginx/src/page/FirstPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const FirstPage = () => {
+  return (
+    <div>FirstPage
+      <Link to="/">Home</Link>
+    </div>
+  )
+}
