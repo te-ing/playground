27a6b76852e6cbcf4afe39b89e98b52e3f59bb18
te-ing <hi2177@naver.com>
2023-07-02 18:37:09 +0900

    study: decorator, redirect

--- nestjs-practice/src/app.module.ts
+++ nestjs-practice/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GeneratorPractice } from './practice/Generator';
+import { RedirectModule } from './redirect/redirect.module';
 
 @Module({
-  imports: [GeneratorPractice],
+  imports: [GeneratorPractice, RedirectModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- nestjs-practice/src/practice/Generator.ts
+++ nestjs-practice/src/practice/Generator.ts
@@ -1,3 +1,9 @@
-console.log('GeneratorPractice');
+import { Module } from '@nestjs/common';
+import { MethodGenerator } from './MethodGenerator';
 
+// console.log('*** GeneratorPractice ***');
+
+@Module({
+  imports: [MethodGenerator],
+})
 export class GeneratorPractice {}
