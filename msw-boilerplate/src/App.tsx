8f5dd60129438f80d93ea3bb7b0d113d7df496dc
taejung.kim <taejung.kim@snplab.io>
2023-08-18 11:07:58 +0900

    msw: Proxy, Reflect

--- msw-boilerplate/src/App.tsx
+++ msw-boilerplate/src/App.tsx
@@ -9,6 +9,40 @@ function App() {
     setRes(result);
   };
 
+  const menu: { [key: string]: number } = {
+    apple: 5000,
+    banana: 2000,
+  };
+
+  // https://soobing.github.io/javascript/metaprogramming/
+  const fruitHandler: ProxyHandler<{ [key: string | symbol]: number }> = {
+    get(target, prop) {
+      if (prop === "사과") return target.apple;
+      if (prop === "바나나") return target.banana;
+      return target[prop];
+    },
+    set(target, prop, value) {
+      try {
+        target[prop] = value;
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  };
+
+  const onClickFruits = async () => {
+    const { data }: { data: string[] } = await axios(`fruits`);
+    console.log(menu[data[0]]);
+  };
+  const onClick과일 = async () => {
+    const { data }: { data: string[] } = await axios(`과일`);
+    const proxyMenu = new Proxy(menu, fruitHandler);
+    console.log(proxyMenu[data[0]]);
+    console.log(Reflect.get(proxyMenu, data[1]));
+  };
+
   return (
     <div style={{ display: "flex", flexDirection: "column", padding: "30px" }}>
       MSW
@@ -16,6 +50,12 @@ function App() {
       <div>
         <button type="button" onClick={() => getJson()}>
           today
+        </button>
+        <button type="button" onClick={onClickFruits}>
+          Fruits
+        </button>
+        <button type="button" onClick={onClick과일}>
+          과일
         </button>
         <button type="button" onClick={() => getJson("20221120")}>
           20221120
--- msw-boilerplate/src/mocks/handlers/fruit.ts
+++ msw-boilerplate/src/mocks/handlers/fruit.ts
@@ -3,7 +3,10 @@ import { rest, RestRequest } from "msw";
 
 // [Get] fruits 서버 요청 시 [ '사과', '바나나' ]를 응답한다.
 
-const fruits = ["사과", "바나나"];
+const 과일 = ["사과", "바나나"];
+const fruits = ["apple", "banana"];
+export const get과일 = rest.get(`/${encodeURIComponent("과일")}`, (req, res, ctx) => res(ctx.json(과일)));
+
 export const getFruits = rest.get("/fruits", (req, res, ctx) => res(ctx.json(fruits)));
 
 export const postFruit = rest.post("/fruits", async (req: RestRequest, res, ctx) => {
