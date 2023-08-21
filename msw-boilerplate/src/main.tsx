5e3bda1998efe173b9ec37ab43b3c3f95e217307
taejung.kim <taejung.kim@snplab.io>
2023-08-21 12:27:34 +0900

    msw: vite --mode에 따른 msw 동작여부 구현

--- msw-boilerplate/src/main.tsx
+++ msw-boilerplate/src/main.tsx
@@ -4,11 +4,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { worker } from "./mocks/browser";
 
-if (window.location.hostname === "localhost")
+if (import.meta.env.MODE === "msw")
   worker.start({
     onUnhandledRequest: "bypass",
   });
-
+console.log("import.meta.env.MODE", import.meta.env.MODE);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
--- msw-boilerplate/vite.config.ts
+++ msw-boilerplate/vite.config.ts
@@ -1,17 +1,17 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@/': `${process.cwd()}/src/`,
+      "@/": `${process.cwd()}/src/`,
     },
   },
   server: {
-    host: '0.0.0.0',
-    open: true,
+    host: "0.0.0.0",
+    // open: true,
   },
   build: {
     sourcemap: true,
