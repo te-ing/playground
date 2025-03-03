477ba7d9e0736334fb9faa3963f0cd6f8df5880a
te-ing <hi2177@naver.com>
2025-03-03 18:06:15 +0900

    사용하지 않는 코드 삭제

--- nestjs-practice/src/practice/Generator.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MethodGenerator } from './MethodGenerator';
-
-// console.log('*** GeneratorPractice ***');
-
-@Module({
-  imports: [MethodGenerator],
-})
-export class GeneratorPractice {}
--- nestjs-practice/src/redirect/redirect.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RedirectController } from './redirect.controller';
-
-@Module({
-  imports: [],
-  controllers: [RedirectController],
-  providers: [],
-})
-export class RedirectModule {}
